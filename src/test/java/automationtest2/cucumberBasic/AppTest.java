65675644cfe9dddb4eb6b476eebf7b9b8ccd4a73
udaykumar4923 <udaykumar@qainfotech.com>
2018-08-06 15:25:36 +0530

    updated

--- src/test/java/automationtest2/cucumberBasic/AppTest.java
+++ src/test/java/automationtest2/cucumberBasic/AppTest.java
@@ -1,38 +1,18 @@
 package automationtest2.cucumberBasic;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "Feature"
+		,glue = {"stepDefinitons"}
+		)
+public class AppTest {
+
 }
